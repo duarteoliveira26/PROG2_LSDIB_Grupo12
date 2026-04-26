0e2cb3cdc12363e1d77476a14aef0033e449c286
rosarinhobarros <up202506863@up.pt>
2026-04-26 16:49:54 +0100

    javadoc indicadores

--- src/interfaces/Indicadores.java
+++ src/interfaces/Indicadores.java
@@ -1,8 +1,23 @@
 package interfaces;
 
 import java.time.LocalDate;
+/**
+ * Interface que define indicadores de ocupação de uma enfermaria.
+ */
 
 public interface Indicadores {
+    /**
+     * Calcula a taxa de ocupação numa data.
+     *
+     * @param data data de referência
+     * @return taxa de ocupação em percentagem
+     */
     double taxaOcupacao(LocalDate data);
+    /**
+     * Verifica se a enfermaria está em pressão.
+     *
+     * @param data data de referência
+     * @return true se ocupação > 85%, false caso contrário
+     */
     boolean emPressao(LocalDate data);
 }
