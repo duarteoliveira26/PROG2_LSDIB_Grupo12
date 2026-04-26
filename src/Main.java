15221a9967601c181c975112302b6805b449ac75
duarteoliveira26 <up202505689@up.pt>
2026-04-26 14:40:14 +0100

    classe main

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import java.io.PrintStream;
+import java.time.LocalDate;
+import model.Enfermaria;
+import model.Hospital;
+import utils.CSVReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Hospital hospital = new Hospital("Hospital XYZ");
+        CSVReader.carregarEnfermarias("src/enfermarias.csv", hospital);
+        CSVReader.carregarEpisodios("src/episodios.csv", hospital);
+        LocalDate hoje = LocalDate.of(2026, 4, 8);
+        LocalDate inicioRelatorio = LocalDate.of(2026, 4, 1);
+        LocalDate fimRelatorio = LocalDate.of(2026, 4, 10);
+        hospital.ordenarPorTaxa(hoje);
+        System.out.println("\n>>> RELATÓRIO DIÁRIO DE OCUPAÇÃO (" + String.valueOf(hoje) + ") <<<");
+        System.out.println("===========================================================================");
+        System.out.printf("%-5s | %-15s | %-10s | %-12s | %-10s\n", "ID", "TIPO", "OCUPAÇÃO", "ESTADO", "DUR. MÉDIA");
+        System.out.println("---------------------------------------------------------------------------");
+
+        for(Enfermaria e : hospital.getEnfermarias()) {
+            String estado = e.emPressao(hoje) ? "!! PRESSÃO !!" : "NORMAL";
+            System.out.printf("%-5s | %-15s | %6.1f%%    | %-12s | %6.1f dias\n", e.getId(), e.getClass().getSimpleName().replace("Enfermaria", ""), e.taxaOcupacao(hoje), estado, e.mediaLoS());
+        }
+
+        PrintStream var10000 = System.out;
+        String var10001 = String.valueOf(inicioRelatorio);
+        var10000.println("\n>>> ANÁLISE DE PERFORMANCE (Período: " + var10001 + " a " + String.valueOf(fimRelatorio) + ") <<<");
+        System.out.println("===========================================================================");
+
+        for(Enfermaria e : hospital.getEnfermarias()) {
+            System.out.println("ENFERMARIA: " + e.getId());
+            System.out.printf("  > LoS: [Mín: %d | Máx: %d | Média: %.1f | Desvio Padrão: %.2f]\n", e.minLoS(), e.maxLoS(), e.mediaLoS(), e.desvioPadrao());
+            double percPressao = e.percentagemPressao(inicioRelatorio, fimRelatorio);
+            System.out.printf("  > Pressão no período: %.1f%% dos dias.\n", percPressao);
+            System.out.println("---------------------------------------------------------------------------");
+        }
+
+        System.out.println("\n[INFO] Verifique o ficheiro 'erros.log' para dados de entrada inválidos.");
+    }
+}
