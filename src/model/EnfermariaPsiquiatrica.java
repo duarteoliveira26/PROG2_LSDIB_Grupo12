7a5cb59c21c06fac002ec8865660cf1ac08e8671
rosarinhobarros <up202506863@up.pt>
2026-04-26 16:12:57 +0100

    Update EnfermariaPsiquiatrica.java

--- src/model/EnfermariaPsiquiatrica.java
+++ src/model/EnfermariaPsiquiatrica.java
@@ -1,8 +1,19 @@
 package model;
+/**
+ * Representa uma enfermaria psiquiátrica.
+ */
 
 public class EnfermariaPsiquiatrica extends Enfermaria {
     private String horarioVisitas;
     private int nivelSeguranca;
+    /**
+     * Cria uma enfermaria psiquiátrica.
+     *
+     * @param id identificador
+     * @param camas número de camas
+     * @param horarioVisitas horário de visitas
+     * @param nivelSeguranca nível de segurança
+     */
 
     public EnfermariaPsiquiatrica(String id, int camas, String horarioVisitas, int nivelSeguranca) {
         super(id, camas);
